528f7a2da1c3e025faaeed9b79fecb0da349bbc2
malinsjoblom <malin.sjoblom@hotmail.com>
2021-10-05 11:29:37 +0200

    added child theme

---
+++ single-butiker.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Botiga
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main <?php echo esc_attr( apply_filters( 'botiga_content_class', '' ) ); ?>">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			do_action( 'botiga_before_single_post_content' );
+
+			get_template_part( 'template-parts/content', 'single' );
+
+			do_action( 'botiga_after_single_post_content' );
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
